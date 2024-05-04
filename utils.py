cf4a3135156eb634c27e6ce343c7318729819c13
CentralLinks17 <109009847+CentralLinks17@users.noreply.github.com>
2024-05-04 23:51:37 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -461,7 +461,7 @@ async def get_shortlink(chat_id, link):
     params = {'api': API,
               'url': url}
 
-    response = requests.get(api_url, params=params)
+    response = requests.get(url, params=params)
 
     # Check if the request was successful
     if response.status_code == 200:
