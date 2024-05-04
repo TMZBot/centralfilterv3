24613e3c26e9f0a10cdf8eeef476e5725d5b7ab4
CentralLinks17 <109009847+CentralLinks17@users.noreply.github.com>
2024-05-04 23:38:43 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -444,7 +444,7 @@ def humanbytes(size):
     return str(round(size, 2)) + " " + Dic_powerN[n] + 'B'
 
 async def get_shortlink(chat_id, link):
-    settings = await get_settings(chat_id) #fetching settings for group
+    settings = await get_settings(chat_id)  # fetching settings for group
     if 'shortlink' in settings.keys():
         URL = settings['shortlink']
     else:
@@ -453,51 +453,36 @@ async def get_shortlink(chat_id, link):
         API = settings['shortlink_api']
     else:
         API = SHORTLINK_API
-    https = link.split(":")[0] #splitting https or http from link
-    if "http" == https: #if https == "http":
+    https = link.split(":")[0]  # splitting https or http from link
+    if "http" == https:  # if https == "http":
         https = "https"
-        link = link.replace("http", https) #replacing http to https
-    if URL == "api.shareus.in":
-        url = f'https://{URL}/shortenedUrl'
-        params = {
-            "token": API,
-            "format": "json",
-            "url": link}
-        try:
-            async with aiohttp.ClientSession() as session:
-                async with session.get(url, params=params, raise_for_status=True, ssl=False) as response:
-                    data = await response.json(content_type="text/html")
-                    if data["status"] == "success":
-                        return data["shortenedUrl"]
-                    else:
+        link = link.replace("http", https)  # replacing http to https
+    url = f'https://tnshort.net/api'
+    params = {'api': API,
+              'url': link}
+
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url, params=params, raise_for_status=True, ssl=False) as response:
+                content_type = response.headers.get('content-type', '').split(';')[0]
+                if content_type == 'application/json':
+                    data = await response.json()
+                    if data["status"] == "error":
                         logger.error(f"Error: {data['message']}")
-                        return f'https://{URL}/shortenedUrl?token={API}&format=json&url={link}'
-        except Exception as e:
-            logger.error(e)
-            return f'https://{URL}/shortenedUrl?token={API}&format=json&url={link}'
-    else:
-        url = f'https://{URL}/api'
-        params = {
-            "api": API,
-            "url": link}
-        try:
-            async with aiohttp.ClientSession() as session:
-                async with session.get(url, params=params, raise_for_status=True, ssl=False) as response:
-                    data = await response.json(content_type="text/html")
-                    if data["status"] == "success":
-                        return data["shortenedUrl"]
+                        return None
                     else:
-                        logger.error(f"Error: {data['message']}")
-                        if URL == 'clicksfly.com':
-                            return f'https://{URL}/api?api={API}&url={link}'
-                        else:
-                            return f'https://{URL}/api?api={API}&url={link}'
-        except Exception as e:
-            logger.error(e)
-            if URL == 'clicksfly.com':
-                return f'https://{URL}/api?api={API}&url={link}'
-            else:
-                return f'https://{URL}/api?api={API}&url={link}'
+                        shortened_url = data['shortenedUrl']
+                        # Add the shortened URL to the final link
+                        final_link = f"https://{URL}/api?api={API}&url={shortened_url}"
+                        return final_link
+                else:
+                    logger.error(f"Attempt to decode JSON with unexpected mimetype: {content_type}")
+                    return None
+
+    except Exception as e:
+        logger.error(e)
+        return None
+
 
 async def get_verify_shorted_link(num, link):
     if int(num) == 1:
