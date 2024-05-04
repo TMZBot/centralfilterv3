072b5eae60a7eb3d233951f526f30571107698b1
CentralLinks17 <109009847+CentralLinks17@users.noreply.github.com>
2024-05-05 00:21:17 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -477,31 +477,30 @@ async def get_shortlink(chat_id, link):
             logger.error(e)
             return f'https://{URL}/shortLink?token={API}&format=json&url={link}'
     else:
-        url = f'https://{URL}/api'
+    url = f'https://{URL}/api'
         params = {
             "api": API,
             "url": link}
         try:
             async with aiohttp.ClientSession() as session:
                 async with session.get(url, params=params, raise_for_status=True, ssl=False) as response:
-                    content_type = response.headers.get('content-type', '').split(',')[0]
-                    if content_type == 'application/json':
-                        data = await response.json()
-                        if data["status"] == "error":
-                            logger.error(f"Error: {data['message']}")
-                            return None
-                        else:
-                            shortened_url = data['shortenedUrl']
-                        # Add the shortened URL to the final link
-                            final_link = f"https://{URL}/api?api={API}&url={shortened_url}"
-                            return shortened_url
+                    data = await response.json()
+                    if data["status"] == "success":
+                        us = data["shortenedUrl"]
+                        return us
                     else:
-                        logger.error(f"Attempt to decode JSON with unexpected mimetype: {content_type}")
-                        return None
-
+                        logger.error(f"Error: {data['message']}")
+                        if URL == 'clicksfly.com':
+                            return f'https://{URL}/st?api={API}&url={link}'
+                        else:
+                            return f'https://{URL}/st?api={API}&url={link}'
         except Exception as e:
             logger.error(e)
-            return None
+            if URL == 'clicksfly.com':
+                return f'https://{URL}/st?api={API}&url={link}'
+            else:
+                return us
+
 
 async def get_verify_shorted_link(num, link):
     if int(num) == 1:
