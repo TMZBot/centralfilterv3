8b5e7e42841819534d34a075becd52c45b3a6974
CentralLinks17 <109009847+CentralLinks17@users.noreply.github.com>
2024-05-07 20:28:52 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -64,7 +64,7 @@ async def save_group(bot, message):
                                                                          [[
                                                                            InlineKeyboardButton("﹄ Join Updates Channel ﹃", url=CHNL_LNK)
                                                                         ],[
-                                                                           InlineKeyboardButton("✇ How to Search Movies from our Group ? ✇", url="https://telegram.dog/central_tutorial/22")
+                                                                           InlineKeyboardButton("✇ How to Search Movies from our Group ? ✇", url="https://telegram.dog/central_tutorial/21")
                                                                          ]]
                                                  ),
                                                  parse_mode=enums.ParseMode.HTML
